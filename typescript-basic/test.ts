c4a0b8d35bf7d832081cdd23fef3deaf65953b7b
Dev-JermoeBaek <superior051@icloud.com>
2017-11-15 17:20:59 +0900

    .

---
+++ typescript-basic/test.ts
@@ -0,0 +1,21 @@
+class Test {
+	public static getTest(): string {
+		return "test";
+	}
+}
+
+Test.getTest();
+
+let n = 0;
+n = 'a';
+
+interface Person {
+	name: string;
+	age: number;
+}
+
+const p: Person = {
+	name: 'Jerome',
+	age: 27
+}
+
